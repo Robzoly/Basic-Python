5ecb317d713994c4b4a168610b2b121cbeb14c98
Robzoly <roberto23zeta@gmail.com>
2022-10-29 16:31:25 -0500

    29/10/22

---
+++ Clase 13 (Bucles I).py
@@ -0,0 +1,7 @@
+for i in [1,2,3]:
+    print("Hola mundo, ¿Qué tal?")
+#i tomará el valor de la cantidad de términos que haya en la Lista del bucle
+
+for ia in ["Primavera", "Verano", "Invierno", "Otoño"]:
+    print(ia)
+#El bucle imprimirá los valores dentro de la lista.
