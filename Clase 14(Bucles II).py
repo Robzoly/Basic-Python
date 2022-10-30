dea7e645811a01e7e88b6370768d962bb3718c5e
Robzoly <roberto23zeta@gmail.com>
2022-10-30 14:58:16 -0600

    Python (Curso)

---
+++ Clase 14(Bucles II).py
@@ -0,0 +1,11 @@
+for i in ["Clase","Salón", 35]:
+    print("Hola mundo")
+
+for owo in [1,2,3]:
+    print(owo)
+
+for ia in ["Dinero", "Efectivo"]:
+    print("Hola", end=" ")
+
+for correo in "ejemplo@correo.com":
+    print("Tacos", end=" ")
